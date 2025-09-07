21246bc5663958421796ec0543114c596ea1ed8e
GenkiSugiyama <sugiyama.genki@gmail.com>
2025-09-07 13:00:54 +0900

    done: add sync.RWMutex

--- http-server/in_memory_player_store.go
+++ http-server/in_memory_player_store.go
@@ -1,17 +1,24 @@
 package main
 
+import "sync"
+
 type InMemoryPlayerStore struct {
+	mc    sync.RWMutex
 	store map[string]int
 }
 
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{sync.RWMutex{}, map[string]int{}}
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mc.RLock()
+	defer i.mc.RUnlock()
 	return i.store[name]
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mc.Lock()
+	defer i.mc.Unlock()
 	i.store[name]++
 }
